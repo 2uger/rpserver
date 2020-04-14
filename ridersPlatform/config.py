cd8a9f91d5e1f3b82a5162243f26b4c724ac1f32
Vladislav Rakitski <rakitski457@gmail.com>
2020-04-14 21:22:02 +0300

    gitignore has been added ...

--- ridersPlatform/config.py
+++ ridersPlatform/config.py
@@ -1,7 +1,8 @@
 class Configuration():
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://thuger_db:thuger_db@localhost/ridersPlatform'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://thuger_db:thuger_db@localhost/ridersPlatform'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/ridersPlatform'
     SECRET_KEY = "SECRET_KEY"
 
 
