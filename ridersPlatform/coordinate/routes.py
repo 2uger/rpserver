8f05eaa3e22e474546296238fe2bc12021f47c13
Thuger <oleg.ignatenko12@gmail.com>
2020-04-17 18:25:39 +0300

    Change

--- ridersPlatform/coordinate/routes.py
+++ ridersPlatform/coordinate/routes.py
@@ -7,19 +7,16 @@ from ridersPlatform.models import Coordinate
 
 @socketio.on('connect')
 def client_connect():
-    print('hello')
-    # emit('message', 'Oleg')
-    # emit('connect', {'data': 'U r connected to websocket'})
+    emit('connect', {'data': 'U r connected to websocket'})
 
 
 @socketio.on('update_coordinates')
 def rewrite_coordinates(updated_coordinates):
-    print(updated_coordinates)
-    # coordinates = Coordinate.query.filter(updated_coordinates['rider_name']).first()
-    # coordinates.from_dict(updated_coordinates)
-    # db.session.add(coordinates)
-    # db.session.commit()
-    # emit('new_coordinates', coordinates.to_dict())
+    coordinates = Coordinate.query.filter(updated_coordinates['rider_name']).first()
+    coordinates.from_dict(updated_coordinates)
+    db.session.add(coordinates)
+    db.session.commit()
+    emit('new_coordinates', coordinates.to_dict())
 
 
 @socketio.on('disconnect')
