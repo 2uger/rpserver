3dfbf6788c537ced283ef3df93b36818999a5305
Vladislav Rakitski <rakitski457@gmail.com>
2020-04-17 18:05:39 +0300

    fix some problems related to connection with socket

--- ridersPlatform/coordinate/routes.py
+++ ridersPlatform/coordinate/routes.py
@@ -7,16 +7,19 @@ from ridersPlatform.models import Coordinate
 
 @socketio.on('connect')
 def client_connect():
-    emit('connect', {'data': 'U r connected to the websocket'})
+    print('hello')
+    # emit('message', 'Oleg')
+    # emit('connect', {'data': 'U r connected to websocket'})
 
 
 @socketio.on('update_coordinates')
 def rewrite_coordinates(updated_coordinates):
-    coordinates = Coordinate.query.filter(updated_coordinates['rider_name']).first()
-    coordinates.from_dict(updated_coordinates)
-    db.session.add(coordinates)
-    db.session.commit()
-    emit('new_coordinates', coordinates.to_dict())
+    print(updated_coordinates)
+    # coordinates = Coordinate.query.filter(updated_coordinates['rider_name']).first()
+    # coordinates.from_dict(updated_coordinates)
+    # db.session.add(coordinates)
+    # db.session.commit()
+    # emit('new_coordinates', coordinates.to_dict())
 
 
 @socketio.on('disconnect')
