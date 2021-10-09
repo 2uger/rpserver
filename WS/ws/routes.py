a9a661ed77f70d5bcf3fe59d1a8b3b7a92595c43
Thuger <oleg.ignatenko12@gmail.com>
2021-10-09 17:07:48 +0300

    Remove whole ws old part

--- WS/ws/routes.py
+++
@@ -1,8 +0,0 @@
-from map.views import Map, WebSocket
-
-
-routes_post = [
-        ('/activate_user_map', Map.activate),
-        ('/deactivate_user_map', Map.deactivate),
-        ('/create_rider/{rider_id}', WebSocket.create),
-]
